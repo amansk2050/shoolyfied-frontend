4701969363e74179fc7b57fabb61289f6b175eb1
shivam-kumar88 <shivam882531@gmail.com>
2023-09-10 19:28:33 +0530

    updated the mainpage

--- src/pages/mainpage/MainPage.js
+++ src/pages/mainpage/MainPage.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import logo from "../../assets/img/HeroBannerimg.png"
+import Header from '../../components/Header'
 
 function MainPage() {
   return (
-    <div>MainPage</div>
+    <div className='main'>
+        <Header/>
+        <img style={{width:500, height:600, objectFit:"contain", marginLeft:"50px"}} src={logo}/>
+    </div>
   )
 }
 
